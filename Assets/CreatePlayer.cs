24f8d4112cb15477a61e4b2fd7a75b7c9e06c6ce
Kevin <550031@student.saxion.nl>
2025-05-08 12:47:08 +0200

    Initial work on InputSystem

---
+++ Assets/CreatePlayer.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CreatePlayer : MonoBehaviour
+{
+    [SerializeField] private GameObject playerPrefab;
+    PlayerInputManager playerInputManager;
+    [SerializeField] private InputActionAsset inputActionAsset;
+    private int numPlayers = 0;
+    private int maxPlayers = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerInputManager = GetComponent<PlayerInputManager>();
+        maxPlayers = playerInputManager.maxPlayerCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && numPlayers < maxPlayers)
+        {
+            PlayerInput.Instantiate(playerPrefab, controlScheme: numPlayers == 0 ? "KeyboardLeft" : "KeyboardRight", pairWithDevice: Keyboard.current);
+            //Instantiate(playerPrefab).GetComponent<PlayerInput>().actions = inputActionAsset;
+            numPlayers++;
+        }
+    }
+}
