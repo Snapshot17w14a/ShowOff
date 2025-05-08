746948af6673175025d4e71d312a271d3dcd5f88
Kevin <550031@student.saxion.nl>
2025-05-08 13:54:41 +0200

    Working camera follow

--- Assets/Scripts/PlayerCenterFollow.cs
+++ Assets/Scripts/PlayerCenterFollow.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerCenterFollow : MonoBehaviour
 {
     private Transform[] playerTransforms;
+    private Vector3 initialOffset;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,24 +14,35 @@ public class PlayerCenterFollow : MonoBehaviour
         {
             playerTransforms[i] = players[i].transform;
         }
+
+        initialOffset = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, )
+        var players = FindObjectsByType<MinigamePlayer>(FindObjectsSortMode.None);
+        playerTransforms = new Transform[players.Length];
+        for (int i = 0; i < players.Length; i++)
+        {
+            playerTransforms[i] = players[i].transform;
+        }
+        transform.position = Vector3.Slerp(transform.position, AvragePlayerPosition(), Time.deltaTime);
     }
 
     private Vector3 AvragePlayerPosition()
     {
         float sumX = 0, sumY = 0, sumZ = 0;
+        float numTransform = playerTransforms.Length;
 
-        foreach(var transform in playerTransforms)
+        foreach (var transform in playerTransforms)
         {
             var pos = transform.position;
             sumX += pos.x;
             sumY += pos.y;
             sumZ += pos.z;
         }
+
+        return new Vector3(sumX / numTransform, sumY / numTransform, sumZ / numTransform) + initialOffset;
     }
 }
