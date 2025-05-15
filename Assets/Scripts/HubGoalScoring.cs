8f5af20c887077a519ea2c9522f09e132e952443
JimmevD <72076148+JimmevD@users.noreply.github.com>
2025-05-15 14:23:34 +0200

    Changed Hub

---
+++ Assets/Scripts/HubGoalScoring.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+using UnityEngine.VFX;
+
+public class HubGoalScoring : MonoBehaviour
+{
+    [Header("Ball")]
+    [SerializeField] private Vector3 ballRespawnPosition;
+    [SerializeField] private GameObject ballPrefab;
+
+    [Header("Assign Please")]
+    [SerializeField] private TextMeshPro scoreText;
+    [SerializeField] private VisualEffect partyParticle;
+    [SerializeField] private AudioSource partySound;
+    private int currentScore; 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Pickupable>()) 
+        {
+            currentScore++;
+            scoreText.text = currentScore.ToString();
+            
+            if (partyParticle)
+                partyParticle.SendEvent("OnPlay");
+
+            if (partySound)
+                partySound.Play();
+
+            StartCoroutine(RespawnBall(other.gameObject));
+        }
+    }
+
+    private IEnumerator RespawnBall(GameObject curBall)
+    {
+        yield return new WaitForSeconds(1f);
+        Destroy(curBall);
+        Instantiate(ballPrefab, ballRespawnPosition, Quaternion.identity);
+    }
+}
