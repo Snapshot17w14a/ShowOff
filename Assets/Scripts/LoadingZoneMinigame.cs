231b033bad97098844f227c5e72ed52c5dcf234c
JimmevD <72076148+JimmevD@users.noreply.github.com>
2025-05-19 14:23:47 +0200

    Loading zone Minigame
    Stand in the zone in the hub with all players to load minigame

---
+++ Assets/Scripts/LoadingZoneMinigame.cs
@@ -0,0 +1,52 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingZoneMinigame : MonoBehaviour
+{
+    [SerializeField] private string minigameName;
+    [SerializeField] private TextMeshProUGUI countDownTimerText;
+
+    private int curPlayersReady;
+    private float countdownTimer = 3;
+    private bool playersReady;
+
+    private void Update()
+    {
+        if (playersReady)
+        {
+            countdownTimer -= Time.deltaTime;
+            countDownTimerText.text = countdownTimer.ToString("F0");
+
+            if (countdownTimer <= 0)
+            {
+                SceneManager.LoadScene(minigameName);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            curPlayersReady++;
+        }
+
+        if (ServiceLocator.GetService<PlayerRegistry>().RegisteredPlayerCount == curPlayersReady && curPlayersReady >= 2)
+        {
+            playersReady = true;
+            countDownTimerText.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            curPlayersReady--;
+            playersReady = false;
+            countdownTimer = 3;
+            countDownTimerText.enabled = false;
+        }
+    }
+}
