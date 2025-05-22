1e4f78c17400c0af933deca45d9e661a1069159a
Kevin <550031@student.saxion.nl>
2025-05-22 13:54:30 +0200

    Platform falling away

--- Assets/Scripts/Minigame Scripts/Icy Showdown Scripts/IcePlatform.cs
+++ Assets/Scripts/Minigame Scripts/Icy Showdown Scripts/IcePlatform.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class IcePlatform : MonoBehaviour
@@ -5,6 +6,11 @@ public class IcePlatform : MonoBehaviour
     [SerializeField] private Color solidColor;
     [SerializeField] private Color brittleColor;
     [SerializeField] private Color waterColor;
+
+    [SerializeField] private float sinkTime = 1f;
+
+    private Vector3 startingPosition;
+    private Vector3 sinkPosition;
 
     private MeshRenderer meshRenderer;
     private MeshCollider meshCollider;
@@ -19,7 +25,9 @@ public class IcePlatform : MonoBehaviour
         meshRenderer = GetComponent<MeshRenderer>();
         meshCollider = GetComponent<MeshCollider>();
 
-        meshRenderer.material.color = solidColor;
+        startingPosition = transform.position;
+
+        sinkPosition = startingPosition + new Vector3(0, -8, 0);
     }
 
     public void FreezePlatform()
@@ -27,6 +35,7 @@ public class IcePlatform : MonoBehaviour
         IsBrittle = false;
         meshCollider.enabled = true;
         meshRenderer.enabled = true;
+
         meshRenderer.material.color = solidColor;
     }
 
@@ -47,12 +56,27 @@ public class IcePlatform : MonoBehaviour
     {
         IsBrittle = false;
         meshCollider.enabled = false;
-        meshRenderer.enabled = false;
-        meshRenderer.material.color = waterColor;
 
         for (int i = 0; i < transform.childCount; i++)
         {
             Destroy(transform.GetChild(i).gameObject);
         }
+
+        StartCoroutine(SinkPlatform());
+    }
+
+    private IEnumerator SinkPlatform()
+    {
+        float time = 0;
+
+        while (time < sinkTime)
+        {
+            time += Time.deltaTime;
+            transform.position = Vector3.Lerp(startingPosition, sinkPosition, time / sinkTime);
+            yield return null;
+        }
+
+        meshRenderer.enabled = false;
+        transform.position = startingPosition;
     }
 }
