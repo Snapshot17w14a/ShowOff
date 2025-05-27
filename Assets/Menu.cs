0541f759d7e18b352a4d056b28810a440c9337aa
Aleksandar <549190@student.saxion.nl>
2025-05-27 13:53:27 +0200

    removed pauseManager stored reference

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -3,27 +3,27 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    private PauseManager pauseManager;
+
     void Awake()
     {
-        pauseManager = ServiceLocator.GetService<PauseManager>();
+
     }
 
     public void Continue()
     {
-
+        ServiceLocator.GetService<PauseManager>().SetIsPaused();
     }
 
     public void Restart()
     {
         Scene scene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(scene.name);
-        pauseManager.SetIsPaused();
+        ServiceLocator.GetService<PauseManager>().SetIsPaused();
     }
 
     public void GoToHub()
     {
         SceneManager.LoadScene("HubScene");
-        pauseManager.SetIsPaused();
+        ServiceLocator.GetService<PauseManager>().SetIsPaused();
     }
 }
