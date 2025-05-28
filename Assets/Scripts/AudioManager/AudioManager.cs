1511e941d8149ae968a98b1c0fe6af401057b4bc
Aleksandar <549190@student.saxion.nl>
2025-05-28 12:13:41 +0200

    Started working on AudioManager

---
+++ Assets/Scripts/AudioManager/AudioManager.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+    [Serializable]
+    public class NameOfAudioClip
+    {
+        public string name;
+        public AudioClip clip;
+    }
+
+    [SerializeField] private List<NameOfAudioClip> audioClips;
+
+    private Dictionary<string, AudioClip> sounds = new Dictionary<string, AudioClip>();
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        foreach (var audioClip in audioClips)
+        {
+            if (!sounds.ContainsKey(audioClip.name))
+            {
+                sounds.Add(audioClip.name, audioClip.clip);
+            }
+            else
+            {
+                Debug.LogError($"Duplicated clip {audioClip.name}");
+            }
+        }
+    }
+
+    public static void PlaySound(string name, float volume = 1f)
+    {
+        if (Instance == null)
+        {
+            return;
+        }
+
+        if (Instance.sounds.TryGetValue(name, out var clip))
+        {
+            Instance.audioSource.PlayOneShot(clip, volume);
+        }
+        else
+        {
+            Debug.LogError($"Sound: {name} not found!");
+        }
+    }
+}
