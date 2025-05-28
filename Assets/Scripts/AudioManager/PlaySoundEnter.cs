02386d4ce7fb209ec2d02f3e4f61b6852f98265c
Aleksandar <549190@student.saxion.nl>
2025-05-28 15:56:32 +0200

    I think I cooked with AudioManager(needs testing)

--- Assets/Scripts/AudioManager/PlaySoundEnter.cs
+++ Assets/Scripts/AudioManager/PlaySoundEnter.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class PlaySoundEnter : StateMachineBehaviour
 {
     [SerializeField] private ESoundType sound;
+    [SerializeField] private string name;
     [SerializeField, Range(0, 1)] private float volume = 1f; 
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+        AudioManager.PlaySound(sound, name, volume);
     }
 }
--- Assets/Scripts/AudioManager/PlaySoundExit.cs
+++ Assets/Scripts/AudioManager/PlaySoundExit.cs
@@ -6,6 +6,6 @@ public class PlaySoundExit : StateMachineBehaviour
     [SerializeField, Range(0, 1)] private float volume = 1f;
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        AudioManager.PlaySound(sound, name, volume);
     }
 }
