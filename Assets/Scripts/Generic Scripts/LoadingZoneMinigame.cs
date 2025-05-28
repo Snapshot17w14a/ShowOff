fef6b1082effcb7f4964a8447588edc236a388fd
JimmevD <72076148+JimmevD@users.noreply.github.com>
2025-05-28 12:55:03 +0200

    Cinematic Intro when loading minigame

--- Assets/Scripts/Generic Scripts/LoadingZoneMinigame.cs
+++ Assets/Scripts/Generic Scripts/LoadingZoneMinigame.cs
@@ -1,18 +1,27 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using TMPro;
+using Unity.Cinemachine;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class LoadingZoneMinigame : MonoBehaviour
 {
     [SerializeField] private string minigameName;
     [SerializeField] private TextMeshProUGUI countDownTimerText;
 
+    [SerializeField] private CinemachineSplineDolly cinemachineSplineDolly;
+    [SerializeField] private Camera MainCamera;
+    [SerializeField] private Camera cutsceneCamera;
+
     private int curPlayersReady;
     private float countdownTimer = 3;
     private bool playersReady;
 
     private bool isTranstitioning = false;
+    private bool isLoadingMinigame = false;
 
+    private List<PlayerInput> allPlayerInputs = new List<PlayerInput>();
     private void Update()
     {
         if (playersReady && !isTranstitioning)
@@ -20,12 +29,31 @@ public class LoadingZoneMinigame : MonoBehaviour
             countdownTimer -= Time.deltaTime;
             countDownTimerText.text = countdownTimer.ToString("F0");
 
-            if (countdownTimer <= 0)
+            if (countdownTimer <= 0 && !isTranstitioning)
             {
-                //SceneManager.LoadScene(minigameName);
                 isTranstitioning = true;
-                TransitionController.Instance.TransitionOut("Icy Showdown");
+                MainCamera.enabled = false;
+                cutsceneCamera.enabled = true;
+                countDownTimerText.enabled = false;
+
+                var autodolly = cinemachineSplineDolly.AutomaticDolly.Method as SplineAutoDolly.FixedSpeed;
+                autodolly.Speed = 0.5f;
+
+                for (int i = 0; i < allPlayerInputs.Count; i++)
+                {
+                    allPlayerInputs[i].DeactivateInput();
+                }
             }
+        }
+
+        if (isTranstitioning)
+        {
+            if (cinemachineSplineDolly.CameraPosition >= 2 && !isLoadingMinigame)
+            {
+                isLoadingMinigame = true;
+                TransitionController.Instance.TransitionOut(minigameName);
+            }
+
         }
     }
 
@@ -34,6 +62,12 @@ public class LoadingZoneMinigame : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             curPlayersReady++;
+
+            PlayerInput curPlayerInput = other.GetComponent<PlayerInput>();
+            if (!allPlayerInputs.Contains(curPlayerInput))
+            { 
+                allPlayerInputs.Add(curPlayerInput);
+            }
         }
 
         if (ServiceLocator.GetService<PlayerRegistry>().RegisteredPlayerCount == curPlayersReady && curPlayersReady >= 2)
