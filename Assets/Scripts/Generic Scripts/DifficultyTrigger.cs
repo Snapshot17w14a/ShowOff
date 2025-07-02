2cefc10756236a14ac82894a1854cc9f17db45cd
JimmevD <72076148+JimmevD@users.noreply.github.com>
2025-07-02 13:11:37 +0200

    Made Fire Bigger, Dash Ui only shows in hard mode

--- Assets/Scripts/Generic Scripts/DifficultyTrigger.cs
+++ Assets/Scripts/Generic Scripts/DifficultyTrigger.cs
@@ -4,6 +4,7 @@ using UnityEngine.VFX;
 public class DifficultyTrigger : MonoBehaviour
 {
     private new Renderer renderer;
+    [SerializeField] private GameObject dashUIIndication;
 
     private void Start()
     {
@@ -26,8 +27,22 @@ public class DifficultyTrigger : MonoBehaviour
         GetComponentInChildren<Animator>().SetFloat("Mood", mood);
         renderer.material.SetFloat("_Blend", mood);
         if(mood == 1)
+        {
             GetComponentInChildren<VisualEffect>().Play();
+            
+            if (dashUIIndication)
+            {
+                dashUIIndication.SetActive(true);
+            }
+        }
         else
+        {
             GetComponentInChildren<VisualEffect>().Stop();
+
+            if (dashUIIndication)
+            {
+                dashUIIndication.SetActive(false);
+            }
+        }
     }
 }
